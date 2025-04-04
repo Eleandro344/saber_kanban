b90d7eee569c6dddc9e05ea9b25a6dcde6f23ddf
ELEADNRO344 <eleandromartins23@hotmail.com>
2025-04-03 21:17:06 -0300

    layout_pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,6 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-
             
           </Routes>
         </div>
